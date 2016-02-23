ad268381fa6d6ca49c45f8086f25d386eb8c815e
Scott Arciszewski <scott@arciszewski.me>
2016-02-23 16:58:00 -0500

    Begin skeleton for unit tests

---
+++ test/unit/PCRETest.php
@@ -0,0 +1,14 @@
+<?php
+
+class PCRETest extends PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+        $this->baseDir = dirname(dirname(__DIR__));
+    }
+
+    public function testBaseRules()
+    {
+
+    }
+}
